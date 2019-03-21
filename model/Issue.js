1b6c4b76a0025dd36fd2de1c4557e52c49432833
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2019-03-21 08:04:24 -0400

    fix update function for when no input is set

--- model/Issue.js
+++ model/Issue.js
@@ -56,26 +56,62 @@ const createIssue = (issue, cb) => {
 const updateIssue = (issue, cb) => {
   const projectId = issue.params.project;
   const issueId = issue.body._id
-  
+  const objCopy = {...issue.body}
+  	/*
+  		remove the _id from objCopy because we need to check
+  		if all other propertys are empty. _id will always
+  		have a value, so we must remove it.
+  	*/ 
+  	delete objCopy._id
+  	let empty = Object.keys(objCopy).every(prop => {
+  		return objCopy[prop] === '';
+  	});
+
   Project.findById(projectId, (err, doc) => {
     if(err) {
       console.log('Issue.js updateIssue error', err);
       cb('Could not updated' + issueId, 400);
     } else {
-      doc.issues.forEach(is => {
-        console.log(is, issue.body)
-        if (is._id == issueId) {
-          is.title = issue.body.issue_title == '' ? is.title : issue.body.issue_title;
-          is.text = issue.body.issue_text == '' ? is.text : issue.body.issue_text;
-          is.author = issue.body.created_by == '' ? is.author : issue.body.created_by;
-          is.assignee = issue.body.assigned_to == '' ? is.assignee : issue.body.assigned_to;
-          is.statusText = issue.body.status_text == '' ? is.statusText : issue.body.status_text;
-          is.updatedOn = new Date();
-        }
-      });
-      doc.markModified('issues');
-      doc.save();
-      cb('successfully updated', 200);
+
+
+    	let filteredIssue = doc.issues.filter(is => {
+	        if (is._id == issueId) {
+	        	return is;
+	          // console.log(is, issue.body)
+	          // is.title = issue.body.issue_title == '' ? is.title : issue.body.issue_title;
+	          // is.text = issue.body.issue_text == '' ? is.text : issue.body.issue_text;
+	          // is.author = issue.body.created_by == '' ? is.author : issue.body.created_by;
+	          // is.assignee = issue.body.assigned_to == '' ? is.assignee : issue.body.assigned_to;
+	          // is.statusText = issue.body.status_text == '' ? is.statusText : issue.body.status_text;
+	          // is.updatedOn = new Date();
+	        }
+	      });
+
+    	let ourIssue = filteredIssue[0];
+    	if (empty) {
+			ourIssue.updatedOn = moment().format('YYYY-MM-DD hh:mm a');
+			console.log(ourIssue);
+    		doc.markModified('issues');
+	      	doc.save();
+    		cb('no updated field sent', 200);
+    	} else {
+	      // doc.issues.forEach(is => {
+	        // if (is._id == issueId) {
+	          // console.log(is, issue.body)
+	          // is.title = issue.body.issue_title == '' ? is.title : issue.body.issue_title;
+	          // is.text = issue.body.issue_text == '' ? is.text : issue.body.issue_text;
+	          // is.author = issue.body.created_by == '' ? is.author : issue.body.created_by;
+	          // is.assignee = issue.body.assigned_to == '' ? is.assignee : issue.body.assigned_to;
+	          // is.statusText = issue.body.status_text == '' ? is.statusText : issue.body.status_text;
+	          // is.updatedOn = new Date();
+	        // }
+	      // });
+	      ourIssue.title = issue.body.issue_title == '' ? ourIssue.title : issue.body.issue_title;
+	      doc.markModified('issues');
+	      doc.save();
+	      cb('successfully updated', 200);
+    	}
+
     }
   })
 }
@@ -83,6 +119,6 @@ const updateIssue = (issue, cb) => {
 module.exports = {
   getAllProjects,
   createProject,
-	createIssue,
+  createIssue,
   updateIssue
 }
