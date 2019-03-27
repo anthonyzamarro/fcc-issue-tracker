20a4c28b7685ef6339bc51ad9767e8164428714c
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2019-03-27 07:41:41 -0400

    finish functional testing. remove chai-dom

--- model/Issue.js
+++ model/Issue.js
@@ -154,6 +154,7 @@ const updateIssue = (issue, cb) => {
 }
 
 const deleteIssue = (issue, cb) => {
+  // console.log('WE HAVE REACHED DELETE', issue)
   const projectId = issue.params.project;
   const issueId = issue.body._id;
   if (issueId == '') {
